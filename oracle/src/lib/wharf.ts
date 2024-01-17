429bb854c940a373821a6582fa5ae204d6ed9504
Aaron Cox <aaron@greymass.com>
2024-01-17 13:51:10 -0800

    Allow environmental for oracle

--- oracle/src/lib/wharf.ts
+++ oracle/src/lib/wharf.ts
@@ -4,7 +4,10 @@ import { WalletPluginPrivateKey } from '@wharfkit/wallet-plugin-privatekey';
 import { Contract as DropsContract } from './contracts/drops';
 export * as DropsContract from './contracts/drops';
 
-const walletPlugin = new WalletPluginPrivateKey('KEY');
+if (!process.env.LOCAL_PRIVATE_KEY) {
+	throw new Error('A private key must be provided in an .env file or on the command line.');
+}
+const walletPlugin = new WalletPluginPrivateKey(process.env.LOCAL_PRIVATE_KEY);
 
 export const url = 'https://jungle4.greymass.com';
 export const client = new APIClient({ url });
