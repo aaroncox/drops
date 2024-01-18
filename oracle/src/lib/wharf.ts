796651491ca17e99df32fa9e308731f73b4ba92c
Aaron Cox <aaron@greymass.com>
2024-01-17 19:49:41 -0800

    Oracle configuration options

--- oracle/src/lib/wharf.ts
+++ oracle/src/lib/wharf.ts
@@ -4,18 +4,26 @@ import { WalletPluginPrivateKey } from '@wharfkit/wallet-plugin-privatekey';
 import { Contract as DropsContract } from './contracts/drops';
 export * as DropsContract from './contracts/drops';
 
-if (!process.env.LOCAL_PRIVATE_KEY) {
-	throw new Error('A private key must be provided in an .env file or on the command line.');
+if (!process.env.ACCOUNT_NAME) {
+	throw new Error('An ACCOUNT_NAME value must be provided in an .env file or on the command line.');
 }
-const walletPlugin = new WalletPluginPrivateKey(process.env.LOCAL_PRIVATE_KEY);
+if (!process.env.PERMISSION_LEVEL) {
+	throw new Error(
+		'An PERMISSION_LEVEL value must be provided in an .env file or on the command line.'
+	);
+}
+if (!process.env.PRIVATE_KEY) {
+	throw new Error('A PRIVATE_KEY value must be provided in an .env file or on the command line.');
+}
+const walletPlugin = new WalletPluginPrivateKey(process.env.PRIVATE_KEY);
 
-export const url = 'https://jungle4.greymass.com';
+export const url = process.env.API_ENDPOINT || 'https://jungle4.greymass.com';
 export const client = new APIClient({ url });
 export const dropsContract: DropsContract = new DropsContract({ client });
 
 export const session: Session = new Session({
 	chain: Chains.Jungle4,
 	walletPlugin,
-	actor: 'wharfkit1111',
-	permission: 'active'
+	actor: process.env.ACCOUNT_NAME,
+	permission: process.env.PERMISSION_LEVEL
 });
